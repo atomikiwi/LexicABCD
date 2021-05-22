ff30c1e4a6baf58392a1e02533dc2c361de5e0f3
Olivier Lefebvre-Montembault <montembault.olivier@gmail.com>
2021-05-22 18:44:13 -0400

    Update Main.java
    finalisation pour travail noté

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,9 @@
 public class Main {
 
     public static void main(String[] args) {
-        //a = 97
-        //b = 98
-        //c = 99
-        //d = 100
         int iterationPrint = 0;
         long startTime = System.currentTimeMillis();
-
         int[] display = {97,97,97,97};
-
         for(int a1 = 97; a1 <101; a1++) {
             for(int a2 = 97; a2 <101; a2++) {
                 for(int a3 = 97; a3 <101; a3++) {
@@ -18,7 +12,6 @@ public class Main {
                         display[1] = a2;
                         display[2] = a3;
                         display[3] = a4;
-
                         // La chaine ne peux pas se terminer par b
                         if(display[3] != 98) {
                             boolean shouldPrint = true;
@@ -32,7 +25,6 @@ public class Main {
                                         break;
                                     }
                                 }
-
                                 //Vérification de la condition: si la chaine contient a ET d
                                 if(display[i] == 97 || display[i] == 100) {
                                     if(display[i] == 97) {
@@ -60,16 +52,13 @@ public class Main {
             }
         }
         String output = "- " + iterationPrint + " chaine(s) de charactères différentes ont été affichées.";
-
         for (int j = 0; j < output.length(); j++) {
             System.out.print("=");
         }
         System.out.println();
         System.out.println("Statistiques du programme:");
         System.out.println(output);
-
         long timelapse = System.currentTimeMillis() - startTime;
         System.out.println("- Le programme s'est terminé en " + timelapse + " milliseconde(s).");
-
     }
 }
